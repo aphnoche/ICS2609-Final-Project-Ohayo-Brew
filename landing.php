410fdc6d3589045c3633d3332e195c1f55af1bab
Alyssa Pauline Noche <alyssapauline19@gmail.com>
2026-05-24 13:44:27 +0800

    i can't run this, i think i'll create a new repo

---
+++ landing.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome to Ohayo Brew!</title>
+        <style>
+        body {
+            background-image: url('landing-bg.png');
+            background-size: cover;
+            background-position: center;
+            height: 100vh;
+            margin: 0;
+        }
+    </style>
+    </head>
+    
+    <body>
+        <div class="container">
+            
+        </div>
+    </body>
+
+</html>
+<?php
+
+?>
